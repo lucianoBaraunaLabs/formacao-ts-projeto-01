77737d339caaee1460b02402cd3db2db86f06c81
Luciano Baraúna <lucianobaraunadev@gmail.com>
2025-11-02 17:36:57 -0300

    finalizada(projeto-01): Domínio: O objeto turma

---
+++ src/domain/Class.ts
@@ -0,0 +1,44 @@
+import z from 'zod'
+import { Serializable } from './types.js'
+import { randomUUID } from 'crypto'
+
+export const ClassCreationSchema = z.object({
+  id: z.uuid().optional(),
+  teacher: z.uuid().nullable(),
+  code: z.regex(/^[0-9]{1}[A-H]{1}-[MTN]$/),
+});
+
+export type ClassCreationType = z.infer<typeof ClassCreationSchema>;
+
+export const ClassUpdateSchema = ClassCreationSchema.partial().omit({id: true});
+
+export type ClassUpdateType = z.infer<typeof ClassUpdateSchema>;
+
+export class Class implements Serializable {
+    code: ClassCreationType['code'];
+    accessor teacher : ClassCreationType['teacher'];
+    readonly id: string;
+
+    constructor(data: ClassCreationType) {
+        const parsedData = ClassCreationSchema.parse(data);
+        this.code = parsedData.code;
+        this.teacher = parsedData.teacher;
+        this.id = parsedData.id ?? randomUUID();
+    }
+
+    static fromObject (obj: Record<string, unknown>): Class {
+        const parsedData = ClassCreationSchema.parse(obj);
+        return new Class(parsedData);
+    }
+    
+    toJSON (): string {
+        return JSON.stringify(this.toObject())
+    }
+    toObject (): Record<string, unknown> {
+        return {
+            id: this.id,
+            teacher: this.teacher,
+            code: this.code,
+        }
+    }
+}
