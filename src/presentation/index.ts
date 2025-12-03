4b3e02071a161cf4313a8bfe7078cdce792fca44
Luciano Baraúna <lucianobaraunadev@gmail.com>
2025-12-03 08:58:10 -0300

    finalizada(projeto-01): Apresentação: Rotas de alunos

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -4,15 +4,16 @@ import { Server } from 'http'
 import { ServiceList } from '../app.js'
 import { AppConfig } from '../config.js'
 import { parentRouterFactory } from './parent.js'
+import { studentRouterFactory } from './student.js'
 export async function WebLayer(config: AppConfig, services: ServiceList) {
     const app = Express()
     let  server: Server | undefined
     app.use(helmet())
     app.use(Express.json())
     app.use('/parents', parentRouterFactory(services.parent, services.student ))
+    app.use('/students', studentRouterFactory(services.student, services.class))
     // app.use('/classes', classRouterFacory())
     // app.use('/teachers', classRouterFacory())
-    // app.use('/students', classRouterFacory())
     app.get('/ping', (_, res) => {
         res.send('pong').end()
     })
