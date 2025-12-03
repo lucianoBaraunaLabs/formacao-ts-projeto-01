e3d5f962d1425bde801bbbd6b9c951d7aa4ad3d9
Luciano Baraúna <lucianobaraunadev@gmail.com>
2025-12-03 08:57:37 -0300

    finalizada(projeto-01): Apresentação: Rotas de alunos

--- src/presentation/student.ts
+++ src/presentation/student.ts
@@ -1,46 +1,85 @@
-import { Database } from '../data/Db.js'
-import { ConflictError } from '../domain/Errors/Conflict.js'
-import { Student, StudentCreationType, StudentUpdateType } from '../domain/Student.js'
-import { Service } from './BaseService.js'
-import { ParentService } from './ParentService.js'
+import { Router, Request } from 'express'
+import {
+  Student,
+  StudentCreationSchema,
+  StudentCreationType,
+  StudentUpdateSchema,
+  StudentUpdateType,
+} from '../domain/Student.js'
+import { StudentService } from '../services/StudentService.js'
+import zodValidationMiddleware from './middlewares/zodValidationMiddleware.js'
+import { Parent } from '../domain/Parent.js'
+import { ClassService } from '../services/ClassService.js'
 
-export class StudentService extends Service {
-  constructor(repository: Database, private readonly parentService: ParentService) {
-    super(repository)
-  }
+export function studentRouterFactory(studentService: StudentService, classService: ClassService) {
+  const router = Router()
 
-  update(id: string, newData: StudentUpdateType) {
-    const entity = this.findById(id) as Student // FIXME: Como melhorar?
-    const updated = new Student({
-      ...entity.toObject(),
-      ...newData
-    })
-    this.repository.save(updated)
-    return updated
-  }
-
-  create(creationData: StudentCreationType) {
-    const existing = this.repository.listBy('document', creationData.document)
-    if (existing.length > 0) {
-      throw new ConflictError(creationData.document, Student)
+  router.get('/:id', async (req, res, next) => {
+    try {
+      const student = studentService.findById(req.params.id)
+      return res.json(student.toObject())
+    } catch (error) {
+      next(error)
     }
-    creationData.parents.forEach((parentId) => this.parentService.findById(parentId))
-    const entity = new Student(creationData)
-    this.repository.save(entity)
-    return entity
-  }
+  })
 
-  getParents(studentId: string) {
-    const student = this.findById(studentId) as Student // FIXME: Como melhorar?
-    return student.parents.map((parentId: string) => this.parentService.findById(parentId))
-  }
+  router.get('/', async (_, res) => {
+    return res.json((studentService.list() as Student[]).map((student: Student) => student.toObject())) // FIXME: Como melhorar?
+  })
 
-  linkParents(id: string, parentsToUpdate: StudentCreationType['parents']) {
-    const student = this.findById(id) as Student // FIXME: Como melhorar?
-    parentsToUpdate.forEach((parentId) => this.parentService.findById(parentId))
+  router.post('/', zodValidationMiddleware(StudentCreationSchema.omit({ id: true })), async (req, res, next) => {
+    try {
+      const student = studentService.create(req.body)
+      // verifica se a classe existe antes de inserir o objeto
+      classService.findById(req.body.class)
+      return res.status(201).json(student.toObject())
+    } catch (error) {
+      next(error)
+    }
+  })
 
-    student.parents = parentsToUpdate
-    this.repository.save(student)
-    return student
-  }
+  router.put(
+    '/:id',
+    zodValidationMiddleware(StudentUpdateSchema),
+    async (req: Request<{ id: string }, any, StudentUpdateType>, res, next) => {
+      try {
+        const { id } = req.params
+        const updated = studentService.update(id, req.body)
+        return res.json(updated.toObject())
+      } catch (error) {
+        next(error)
+      }
+    },
+  )
+
+  router.delete('/:id', async (req, res) => {
+    studentService.remove(req.params.id)
+    return res.status(204).send()
+  })
+
+  router.get('/:id/parents', async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const parents = studentService.getParents(id) as Parent[] // FIXME: Como melhorar?
+      return res.json(parents.map((parent: Parent) => parent.toObject()))
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.patch(
+    '/:id/parents',
+    zodValidationMiddleware(StudentCreationSchema.pick({ parents: true })),
+    async (req: Request<{ id: string }, any, Pick<StudentCreationType, 'parents'>>, res, next) => {
+      try {
+        const { id } = req.params
+        const { parents } = req.body
+        return res.json(studentService.linkParents(id, parents).toObject())
+      } catch (error) {
+        next(error)
+      }
+    },
+  )
+
+  return router
 }
